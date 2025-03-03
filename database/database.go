b1d4608e643891775e599673db3c282cbb6f0f25
Bojan Todorovic <bojan.todorovic@HH-A-080.local>
2025-03-03 22:36:52 +0100

    nove izmene

--- database/database.go
+++ database/database.go
@@ -3,29 +3,42 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 
-	_ "github.com/lib/pq" // PostgreSQL drajver
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq" // PostgreSQL driver
 )
 
-var DB *sql.DB // Globalna varijabla za bazu
+var DB *sql.DB
 
-// Funkcija za povezivanje sa bazom
 func ConnectDB() error {
+	// Učitaj promenljive iz .env fajla
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Neuspešno učitavanje .env fajla", err)
+	}
+
+	// Učitaj vrednosti iz environment varijabli
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+
 	// Definiši string za konekciju prema bazi
-	connStr := "user=admin password=Ste11@2016 dbname=elearning sslmode=disable"
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", dbUser, dbPassword, dbName, dbSSLMode)
 	var err error
 	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
 		return fmt.Errorf("Neuspešno povezivanje sa bazom: %v", err)
 	}
 
-	// Testiraj konekciju sa bazom
+	// Proveri da li je baza dostupna
 	err = DB.Ping()
 	if err != nil {
 		return fmt.Errorf("Neuspešno povezivanje sa bazom: %v", err)
 	}
 
-	fmt.Println("Povezan sa bazom!")
+	fmt.Println("Uspešno povezivanje sa bazom.")
 	return nil
-
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,7 @@ require (
 	golang.org/x/crypto v0.35.0
 )
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
